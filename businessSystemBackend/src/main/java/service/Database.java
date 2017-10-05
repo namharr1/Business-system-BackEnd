e26fc88e1065eaf0886c8bc7c6c57cbc43c3ed79
namharr1 <rexhepi.rrahman@hotmail.com>
2017-10-05 14:11:26 +0200

    Need to Test it also add the correct Db name

--- businessSystemBackend/src/main/java/service/Database.java
+++ businessSystemBackend/src/main/java/service/Database.java
@@ -1,5 +1,27 @@
 package service;
 
-public class Database {
+import java.sql.DriverManager;
 
+import com.mysql.jdbc.Connection;
+
+public class Database {
+	public Connection getConnection() throws Exception {
+		try {
+			String connectionURL = "jdbc:mysql://localhost:3306/LäggInDBNamn";
+			String connectionUsername = "root";
+			String connectionPassword = "root";
+			Connection connection = null;
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			connection = (Connection) DriverManager.getConnection(connectionURL, connectionUsername, connectionPassword);
+			if (connection != null) {
+				System.out.println("Connection Successfull to DB: " + connectionURL);
+			} else {
+				System.out.println("Connection Failed to DB: " + connectionURL);
+			}
+			return connection;
+		} 
+		catch (Exception ex) {
+			throw ex;
+		}
+	}
 }
