a2af75f23cb45e443c8fb40e4c0447a70ce03d25
namharr1 <rexhepi.rrahman@hotmail.com>
2017-10-05 14:34:18 +0200

    Created Item service with all the methods, and then autoGenerated the Item impl . not done yet

--- businessSystemBackend/src/main/java/items/ItemService.java
+++ businessSystemBackend/src/main/java/items/ItemService.java
@@ -1,5 +1,21 @@
 package items;
 
-public class ItemService {
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
+import model.Item;
+
+public interface ItemService {
+	List<Item> getAllItems(Connection conn) throws SQLException;
+	List<Item> getAllItemsByCategory(Connection conn) throws SQLException;
+	List<Item> getAllItemsBySupplier(Connection conn) throws SQLException;
+	
+	Item getItemByName(Connection conn, String itemName) throws SQLException;
+	Item getItemByID(Connection conn, long itemID) throws SQLException;
+	
+	void postItem(Connection conn, Item item) throws SQLException;
+	void updateItem(Connection conn, Item item) throws SQLException;
+	void deleteItem(Connection conn, Item item) throws SQLException;
+	
 }
--- businessSystemBackend/src/main/java/items/ItemsImpl.java
+++ businessSystemBackend/src/main/java/items/ItemsImpl.java
@@ -1,5 +1,59 @@
 package items;
 
-public class ItemsImpl {
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import model.Item;
+
+public class ItemsImpl implements ItemService {
+
+	@Override
+	public List<Item> getAllItems(Connection conn) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Item> getAllItemsByCategory(Connection conn) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Item> getAllItemsBySupplier(Connection conn) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item getItemByName(Connection conn, String itemName) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item getItemByID(Connection conn, long itemID) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void postItem(Connection conn, Item item) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateItem(Connection conn, Item item) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteItem(Connection conn, Item item) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
