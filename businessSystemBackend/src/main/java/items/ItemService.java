1e545e4d2d54901823c0aaaaf52fe8c864f6e6fd
namharr1 <rexhepi.rrahman@hotmail.com>
2017-10-10 14:38:49 +0200

    had to add search parameters

--- businessSystemBackend/src/main/java/items/ItemService.java
+++ businessSystemBackend/src/main/java/items/ItemService.java
@@ -8,8 +8,8 @@ import model.Item;
 
 public interface ItemService {
 	List<Item> getAllItems(Connection conn) throws SQLException;
-	List<Item> getAllItemsByCategory(Connection conn) throws SQLException;
-	List<Item> getAllItemsBySupplier(Connection conn) throws SQLException;
+	List<Item> getAllItemsByCategory(Connection conn, String searchCategory) throws SQLException;
+	List<Item> getAllItemsBySupplier(Connection conn, String searchSupplier) throws SQLException;
 	
 	Item getItemByName(Connection conn, String itemName) throws SQLException;
 	Item getItemByID(Connection conn, long itemID) throws SQLException;
