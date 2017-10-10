28141123e4002a4e21cf0b6f1b1abbdc02feea4f
namharr1 <rexhepi.rrahman@hotmail.com>
2017-10-10 16:08:45 +0200

    Added post item

--- businessSystemBackend/src/main/java/items/ItemsImpl.java
+++ businessSystemBackend/src/main/java/items/ItemsImpl.java
@@ -38,12 +38,16 @@ public class ItemsImpl implements ItemService {
 	 that got the same category as the search word. */
 	@Override
 	public List<Item> getAllItemsByCategory(Connection conn, String searchCategory) throws SQLException {
+		System.out.println("getAllItemsByCategory ->");
+		
 		List<Item> items = new LinkedList<Item>();
 		String query = "SELECT * FROM items WHERE Item_category = ?";
 		
 		PreparedStatement statement = conn.prepareStatement(query);
 		statement.setString(1, searchCategory);
 		ResultSet result = statement.executeQuery(query);
+		
+		System.out.println("Search Query: " + query);
 		
 		while (result.next()) {
 			items.add(new Item(result.getInt("ItemId"),result.getString("Item_name"),result.getLong("Item_quantity"),result.getDouble("Item_price"),result.getString("Item_category"),result.getDouble("Item_weight"),result.getString("Item_supplier"),result.getBoolean("Item_unpacked"),result.getLong("Item_totalSold")));
@@ -52,6 +56,7 @@ public class ItemsImpl implements ItemService {
 			statement.close();
 		}
 		
+		System.out.println("Search done: Size of list: " + items.size() + "\ngetAllItemsByCategory: <-");
 		return items;
 	}
 
@@ -60,6 +65,8 @@ public class ItemsImpl implements ItemService {
 	  that got the same supplier as the search word. */
 	@Override
 	public List<Item> getAllItemsBySupplier(Connection conn, String searchSupplier) throws SQLException {
+		System.out.println("getAllItemsBySupplier ->");
+		
 		List<Item> items = new LinkedList<Item>();
 		String query = "SELECT * FROM items WHERE Item_supplier = ?";
 		
@@ -74,12 +81,15 @@ public class ItemsImpl implements ItemService {
 			statement.close();
 		}
 		
+		System.out.println("Search Done: Size: " + items.size() + "\ngetAllItemsBySupplier <-");
 		return items;
 	}
 	
 	// Get the item by searching for the name. Returns the item with matching name.
 	@Override
 	public Item getItemByName(Connection conn, String itemName) throws SQLException {
+		System.out.println("getItemByName ->");
+		
 		String query = "SELECT * FROM Items WHERE Item_name = ?";
 		Item item = null;
 		
@@ -87,23 +97,60 @@ public class ItemsImpl implements ItemService {
 		statement.setString(1, itemName);
 		ResultSet result = statement.executeQuery();
 		
+		System.out.println("Searching query: " + statement);
+		
 		if (result.next()) {
 			item = new Item(result.getInt("ItemId"),result.getString("Item_name"),result.getLong("Item_quantity"),result.getDouble("Item_price"),result.getString("Item_category"),result.getDouble("Item_weight"),result.getString("Item_supplier"),result.getBoolean("Item_unpacked"),result.getLong("Item_totalSold"));
 		}
+		
+		System.out.println("Result: " + item.toString() + "\ngetItemByName <-");
 		return item;
 	}
 	
 	// Get the item by searching for the ID. Returns the item with matching ID.
 	@Override
 	public Item getItemByID(Connection conn, long itemID) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.println("getItemByID ->");
+		
+		String query = "SELECT * FROM Items WHERE Item_ID = ?";
+		Item item = null;
+		
+		PreparedStatement statement = conn.prepareStatement(query);
+		statement.setLong(1, itemID);
+		ResultSet result = statement.executeQuery();
+		
+		System.out.println("Searching Query: " + statement);
+		
+		if (result.next()) {
+			item = new Item(result.getInt("ItemId"),result.getString("Item_name"),result.getLong("Item_quantity"),result.getDouble("Item_price"),result.getString("Item_category"),result.getDouble("Item_weight"),result.getString("Item_supplier"),result.getBoolean("Item_unpacked"),result.getLong("Item_totalSold"));
+		}
+		
+		System.out.println("Result: " + item.toString() + "\ngetItemByID <-");
+		return item;
 	}
 	
 	// Add new item to the database. 
 	@Override
 	public void postItem(Connection conn, Item item) throws SQLException {
-		// TODO Auto-generated method stub
+		System.out.println("PostItem ->");
+		
+		String query = "INSERT INTO Item(Item_name,Item_quantity,Item_price,Item_category,Item_weight,Item_supplier,Item_unpacked,Item_totalSold) VALUES (?,?,?,?,?,?,?,?)";
+		
+		PreparedStatement statement = conn.prepareStatement(query);
+		statement.setString(1, item.getName());
+		statement.setLong(2, item.getQuantity());
+		statement.setDouble(3, item.getPrice());
+		statement.setString(4, item.getCategory());
+		statement.setDouble(5, item.getWeight());
+		statement.setString(6, item.getSupplier());
+		statement.setBoolean(7, item.isUnpacked());
+		statement.setLong(8, item.getTotalSold());
+		statement.executeUpdate();
+		
+		if (!statement.getConnection().isClosed()) {
+			statement.close();
+		}
+		System.out.println("Posted Item: " + item.toString() + "\nPostItem <-");
 		
 	}
 	
